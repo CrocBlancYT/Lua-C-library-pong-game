5943167edcd5fb589b0fff18a6a4178ea2aea9ce
CrocBlanc <85250446+CrocBlancYT@users.noreply.github.com>
2025-02-01 23:49:58 +0100

    Add files via upload

--- src/init.lua
+++ src/init.lua
@@ -3,32 +3,31 @@ local graphics = assert(require('graphics'))
 local function default(_, _) end --_ to silence
 
 local function init(name, width, height)
-    local window = graphics.setupWindow(name, width, height)
+    graphics.initScreen(name, width, height)
 
     local onEvent = default
     local onFrame = default
 
-    local core = {
-        _HWND = window._HWND,
-        _HDC = window._HDC,
-
+    local core = {        
         pixels_wrt_cm = 37.795, --37 pixels = 1cm
 
         runFrame = function (fps)
-            local deltaTime = graphics.wait(1/fps)
+            local deltaTime = graphics.pWait(1/fps)
 
             --clear screen to redraw
-            graphics.clear(window)
+            graphics.clearScreen()
 
             --process events
             while true do
-                local event = graphics.runEventAsync()
+                local event = graphics.getEventAsync()
                 if not event then break end
                 onEvent(event, deltaTime)
             end
 
             --redraw
             onFrame(deltaTime)
+
+            graphics.refreshScreen()
         end,
         
         connectEvent = function (new_onEvent)
